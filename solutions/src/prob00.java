e39db004b5cbe712017bfe87447a6805c8bcd159
Arnav Gudibande <arnav.gudibande@gmail.com>
2017-02-17 12:43:48 -0800

    Added prob00.java

---
+++ solutions/src/prob00.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by arnavgudibande on 2/17/17.
+ */
+public class prob00 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("The Force is strong with " + name);
+    }
+}
