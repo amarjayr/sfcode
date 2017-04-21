4174b15126db905f57a522658770388424770051
Amar Ramachandran <amarrmchndrn1@gmail.com>
2017-04-21 14:25:44 -0700

    Deleted old files

--- README.md
+++
@@ -1,8 +0,0 @@
-# sfcode
-*by [sfhacks](http://www.sfhacks.club/)*
-
-Saint Francis High School Programming Invitational
-
-April 22nd, 2017
-
-Website: [sfcode.sfhacks.club](http://sfcode.sfhacks.club)
--- solutions/src/task00.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Task00 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        System.out.println("The Force is strong with " + name);
-    }
-}
